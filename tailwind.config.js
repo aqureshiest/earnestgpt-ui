1ef5a35e66ae3d75c8ff83a5e240b7b426d2c314
Dalia De Felipe <dvargas@earnest.com>
2023-07-13 15:10:57 -0700

    Earnest gpt/update styles (#3)
    * :lipstick: - update styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,8 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
-        "user-bubble": '#1e0032',
-        "bot-bubble": "#e7daff"
+        "user-bubble": '#1E0033',
+        "bot-bubble": "#F0EDF6"
       },
     },
   },
